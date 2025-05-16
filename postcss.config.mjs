0c0f4947b58451e992459ef519065ebdf36b3696
Vinicius <m.vinicius.s.f.c@gmail.com>
2025-05-15 22:25:52 -0300

    ajuste

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,6 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // geralmente você também quer o autoprefixer
-  },
-};
-
-// ou
-
-// postcss.config.mjs
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+ // postcss.config.mjs
+    export default {
+      plugins: {
+        '@tailwindcss/postcss': {},
+      },
+    };
