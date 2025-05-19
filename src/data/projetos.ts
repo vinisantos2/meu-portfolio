2a3ccccca80d00a30e3b8d19b4e3674cfc55cd1b
Vinicius <m.vinicius.s.f.c@gmail.com>
2025-05-19 15:47:55 -0300

    adicionar o projeto loja-virtual

--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -5,21 +5,24 @@ export const projetos: Projeto[] = [
   {
     title: "Pongada",
     description: "Jogo de tabuleiro 3x3 feito com React Native e TypeScript. Possui animações e modo multijogador.",
-    imagesUrl:[ "/images/pongada/tela-home.png", "/images/pongada/tela-jogo-2-pessoas.png", "/images/pongada/tela-jogo-ia.png"],
+    imagesUrl: ["/images/pongada/tela-home.png", "/images/pongada/tela-jogo-2-pessoas.png", "/images/pongada/tela-jogo-ia.png"],
     projectUrl: "https://github.com/vinisantos2/pongada"
   },
-  
+
   {
     title: "Loteria resultados",
     description: "Aplicativo para consultar resultados de loterias e para comparar um jogo com os ja sorteados",
-    imagesUrl: ["/images/loteria-resultados/resultados.png","/images/loteria-resultados/comparar.png", "/images/loteria-resultados/estatisticas.png"],
+    imagesUrl: ["/images/loteria-resultados/resultados.png", "/images/loteria-resultados/comparar.png", "/images/loteria-resultados/estatisticas.png"],
     projectUrl: "https://github.com/vinisantos2/loteria-react-native"
   },
-  
+
   {
     title: "Magalu clone",
     description: "Aplicativo de vitrine de produtos inspirado no Magazine Luiza. Desenvolvido com React Native e Expo, exibe imagem, nome, preço e botão de compra com layout limpo e responsivo.",
-    imagesUrl: ["/images/magalu-clone/home.png","/images/magalu-clone/produto.png"],
+    imagesUrl: [
+      "/images/magalu-clone/home.png",
+      "/images/magalu-clone/produto.png"
+    ],
     projectUrl: "https://github.com/vinisantos2/magaluClone"
   },
   {
@@ -28,5 +31,17 @@ export const projetos: Projeto[] = [
     imagesUrl: ["/images/calculadora/calculadora.png"],
     projectUrl: "https://github.com/vinisantos2/calculadoraIOS"
   },
-  
+  {
+    title: "Loja Virtual com Next.js e Firebase",
+    description: "Projeto de e-commerce com autenticação, listagem de produtos em tempo real via Firebase, carrinho de compras com Context API e checkout. Desenvolvido com Next.js (App Router), Firestore e Tailwind CSS, utilizando TypeScript e estrutura modular.",
+    imagesUrl: [
+      "/images/loja-virtual/home.png",
+      "/images/loja-virtual/produtos.png",
+      "/images/loja-virtual/detalhes.png",
+      "/images/loja-virtual/admin.png"
+    ],
+    projectUrl: "https://github.com/vinisantos2/loja-virtual"
+  }
+
+
 ];
