f73180d9e1cb38bffdc67010491b64ed6c36f8e9
Vinicius <m.vinicius.s.f.c@gmail.com>
2025-12-16 22:24:51 -0300

    adicionado analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 // pages/_app.tsx
-import Layout from '../components/Layout';
-import MetaHead from '../components/MetaHead';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { Analytics } from "@vercel/analytics/next";
+import Layout from "../components/Layout";
+import MetaHead from "../components/MetaHead";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout title="Home | Meu Portfólio">
       <MetaHead />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
-
   );
 }
