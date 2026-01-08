7fc471e62f0837f3b475d45a1e94a0500e8309a8
Vinicius <m.vinicius.s.f.c@gmail.com>
2026-01-08 17:59:44 -0300

    feat: melhora SEO e textos da home para atrair clientes

--- src/components/MetaHead.tsx
+++ src/components/MetaHead.tsx
@@ -3,8 +3,11 @@ import Head from "next/head";
 export default function MetaHead() {
   return (
     <Head>
-      <title>Meu portifolio</title>
-      <meta name="description" content="Protifólio dos meus projetos." />
+      <title>Desenvolvimento Web e Aplicativos | VS Web Apps</title>
+      <meta
+        name="description"
+        content="Criação de sites e aplicativos modernos, rápidos e responsivos. Desenvolvedor especializado em React, Next.js e React Native para seu negócio crescer online."
+      />
       <meta
         name="google-site-verification"
         content="-qTHmhzHdpxBxu77UD6NHOOJUUTjfmxWsskE6ZkU1FY"
